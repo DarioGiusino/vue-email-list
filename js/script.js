ee09e6c1f727ab5d72ce28141bde45b85375e277
DarioGiusino <dario.giusino@gmail.com>
2023-01-18 15:28:49 +0100

    generated 10 email and printed on page

--- js/script.js
+++ js/script.js
@@ -1,13 +1,39 @@
 // // console.log('vue', Vue);
 
+// Initialize Vue App
 const app = Vue.createApp({
+    // App name
     name: 'Mail List',
     data() {
         return {
             mailList: [],
-            mailApi: 'https://flynn.boolean.careers/exercises/api/random/mail'
+            mailAPI: 'https://flynn.boolean.careers/exercises/api/random/mail'
         }
+    },
+    methods: {
+        // Get a random email from mailAPI
+        getRandomMail() {
+            // Using axios...
+            axios.get(this.mailAPI)
+            .then(response => {
+                // Packed in a constant the server response
+                const newMail = response.data.response;
+
+                // Push the random mail on the data array
+                this.mailList.push(newMail);
+            })
+        }
+    },
+    // Upon page starting...
+    mounted() {
+        // For 10 times...
+        for(let i = 0; i < 10; i++){
+            // Execute the method
+            this.getRandomMail()
+        }
+        
     }
 });
 
+// Mounting the Vue app on HTML tag
 app.mount('#app')
