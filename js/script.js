395526f81c70714f13d4d9d7dabc3634ab825357
DarioGiusino <dario.giusino@gmail.com>
2023-01-18 15:08:54 +0100

    structured html and vue data

--- js/script.js
+++ js/script.js
@@ -4,7 +4,8 @@ const app = Vue.createApp({
     name: 'Mail List',
     data() {
         return {
-
+            mailList: [],
+            mailApi: 'https://flynn.boolean.careers/exercises/api/random/mail'
         }
     }
 });
